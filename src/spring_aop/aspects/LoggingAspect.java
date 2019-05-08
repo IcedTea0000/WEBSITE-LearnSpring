4ba540c03de056b203d91c2b21fae613bedb22fa
Lam Ngo <ngohoanglam65@gmail.com>
2019-05-08 16:44:09 +0700

    Wildcard annotation example

--- src/spring_aop/aspects/LoggingAspect.java
+++ src/spring_aop/aspects/LoggingAspect.java
@@ -2,13 +2,24 @@ package spring_aop.aspects;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
 
-	@Before("execution(public String getName())")
-	public void LoggingActivity(){
-		System.out.println("Logging aspect executed. Logging activity.");
+	@Before("execution(* get*(..))")
+	public void loggingActivity(){
+		System.out.println("logging aspect executed. Logging activity.");
 	}
 	
+	@Before("execution(* get*(..))")
+	public void secondActivity(){
+		System.out.println("second aspect executed.");
+	}
+	
+/*	
+	@Pointcut("execution(* get*(..))")
+	public void allGetter(){
+	}
+*/	
 }
