ee35764860d27b9be2bd31f11e935cfe5b2a4bb8
Lam Ngo <ngohoanglam65@gmail.com>
2019-05-09 16:03:20 +0700

    @Around example

--- src/spring_aop/aspects/LoggingAspect.java
+++ src/spring_aop/aspects/LoggingAspect.java
@@ -1,15 +1,17 @@
 package spring_aop.aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LoggingAspect {
-
+/*
 	@Before("execution(* get*(..))")
 	public void loggingActivity(JoinPoint joinPoint){
 		System.out.println("logging aspect executed.");
@@ -17,22 +19,26 @@ public class LoggingAspect {
 		
 	}
 	
-	@AfterReturning("args(name)")
-	public void stringArgumentMethods(String name){
-		System.out.println("method takes String argument is called successfully, name is "+name);
-	}
-	
-	@AfterThrowing("args(name)")
-	public void stringArgumentMethods2(String name){
-		System.out.println("method throws exception");
-	}
-
-	
 	@Before("execution(* get*(..))")
 	public void secondActivity(){
 		System.out.println("second aspect executed.");
 	}
+*/	
+	@Around("execution(* get*(..))")
+	public void aroundAdvice(ProceedingJoinPoint pjd){
+		
+		try {
+			System.out.println("aspect before method executed");
+			pjd.proceed();
+			System.out.println("aspect after method executed");
 	
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			System.out.println("aspect if method thorws exception executed");
+			e.printStackTrace();
+		}
+		System.out.println("final aspect executed");
+	}
 	
 /*	
 	@Pointcut("execution(* get*(..))")
--- src/spring_aop/main/AopMain.java
+++ src/spring_aop/main/AopMain.java
@@ -3,6 +3,7 @@ package spring_aop.main;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import spring_aop.model.Triangle;
 import spring_aop.service.ShapeService;
 
 public class AopMain {
@@ -11,9 +12,9 @@ public class AopMain {
 	
 	ApplicationContext ctx=new ClassPathXmlApplicationContext("spring.xml");
 	ShapeService shapeService=(ShapeService)ctx.getBean("service");
-	shapeService.getCircle().setName("testName");
-	//System.out.println(shapeService.getCircle().getName());
-	//System.out.println(shapeService.getTriangle().getName());
-	
+	//shapeService.getCircle().setName("testName");
+	//System.out.println("circle name is "+shapeService.getCircle().getName());
+	Triangle t=shapeService.getTriangle();
+	String k=t.getName();
 	}
 }
